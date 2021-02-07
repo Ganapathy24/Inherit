365e39ef504290b5712081645de8f722b1a4b799
Sundara Ganapathy L <51270860+Ganapathy24@users.noreply.github.com>
2021-02-07 16:19:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // // Catch all other invalid routes
 app.get('/', function(req,res){
-    res.status(200).sendFile(__dirname + '/dist/index.html');
+    res.status(200).sendFile('/dist/index.html');
 });
 
 // app.get('/', (req, res) => {
