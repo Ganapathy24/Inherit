1cdd62eddb7da229a7595344c3add221c2245fbc
Sundara Ganapathy L <sundaraganapathy24@gmail.com>
2021-02-17 00:30:44 +0530

    Project uploading page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-
+import {NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {LoginComponent} from './login/login.component';
@@ -14,6 +14,7 @@ import {MatPaginatorModule} from '@angular/material/paginator';
 import {MatDialogModule} from '@angular/material/dialog';
 import { ProjectDetailsComponent } from './project-details/project-details.component';
 import { ImportProjectComponent } from './import-project/import-project.component';
+import {MatChipsModule} from '@angular/material/chips';
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,20 +23,23 @@ import { ImportProjectComponent } from './import-project/import-project.componen
     ToolbarComponent,
     ProjectDialog,
     ProjectDetailsComponent,
-    ImportProjectComponent
+    ImportProjectComponent,
   ],
-  imports: [
-    BrowserModule,
-    CustomMaterialModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    FontAwesomeModule,
-    MatPaginatorModule
-  ],
+    imports: [
+        BrowserModule,
+        DragDropModule,
+        CustomMaterialModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        FontAwesomeModule,
+        MatPaginatorModule,
+        MatChipsModule
+    ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule {
 }
--- src/app/import-project/import-project.component.ts
+++ src/app/import-project/import-project.component.ts
@@ -1,15 +1,110 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {MatChipInputEvent} from '@angular/material/chips';
+import { Directive, Output, Input, EventEmitter, HostBinding, HostListener } from '@angular/core';
+
+export interface Languages {
+  name: string;
+}
 
 @Component({
   selector: 'app-import-project',
   templateUrl: './import-project.component.html',
   styleUrls: ['./import-project.component.scss']
 })
+
+
 export class ImportProjectComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
+
+  projectName: string;
+  description: string;
+  uploadFiles: any;
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  languagesUsed: Languages[] = new Array();
+
+  files: any = [];
+
+  // tslint:disable-next-line:no-output-on-prefix
+  @Output() onFileDropped = new EventEmitter<any>();
+
+  @HostBinding('style.background-color') private background = '#f5fcff';
+  @HostBinding('style.opacity') private opacity = '1';
+
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add our fruit
+    if ((value || '').trim()) {
+      this.languagesUsed.push({name: value.trim()});
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  remove(fruit: Languages): void {
+    const index = this.languagesUsed.indexOf(fruit);
+
+    if (index >= 0) {
+      this.languagesUsed.splice(index, 1);
+    }
+  }
 
   ngOnInit(): void {
   }
 
+  // Dragover listener
+  @HostListener('dragover', ['$event']) onDragOver(evt: { preventDefault: () => void; stopPropagation: () => void; }) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    this.background = '#9ecbec';
+    this.opacity = '0.8';
+  }
+
+  // Dragleave listener
+  @HostListener('dragleave', ['$event'])
+  public onDragLeave(evt: { preventDefault: () => void; stopPropagation: () => void; }) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    this.background = '#f5fcff';
+    this.opacity = '1';
+  }
+
+  // Drop listener
+  @HostListener('drop', ['$event'])
+  public ondrop(evt: { preventDefault: () => void; stopPropagation: () => void; dataTransfer: { files: any; }; }) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    this.background = '#f5fcff';
+    this.opacity = '1';
+    const files = evt.dataTransfer.files;
+    if (files.length > 0) {
+      this.onFileDropped.emit(files);
+    }
+  }
+
+  uploadFile(event: string | any[]): void {
+    for (let index = 0; index < event.length; index++) {
+      const element = event[index];
+      this.files.push(element.name);
+    }
+  }
+
+  deleteAttachment(index: any): void {
+    this.files.splice(index, 1);
+  }
+
 }
+
+
+
