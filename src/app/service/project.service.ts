e689ba78fffc5af91f7a77ad752f5ff4e7f166f3
Sundara Ganapathy L <sundaraganapathy24@gmail.com>
2021-04-06 21:26:37 +0530

    Oru alavuku done.

---
+++ src/app/service/project.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectService {
+  url = 'http://inheritproject.herokuapp.com/api/projects/';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getProjects() {
+    console.log('Getting projects');
+    const data: any = {};
+    const queryselect: any = {};
+    queryselect['languages'] = 'Javascript';
+    data['query'] = queryselect;
+    data['start'] = 0;
+    data['limit'] = 10;
+    console.log(data);
+    return this.http.post(this.url + 'get', data);
+
+  }
+}
